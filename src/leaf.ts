bfe12396b16ce5ccc80d88542e175d84d51cbdaa
Zicklag <zicklag@katharostech.com>
2025-03-08 10:03:33 -0600

    feat: draft entity / component API.

--- src/leaf.ts
+++ src/leaf.ts
@@ -1,8 +1,23 @@
 import decodeBase32 from "base32-decode";
 import encodeBase32 from "base32-encode";
 
-export type ComponentId = string;
-export type ComponentNames = { [name: string]: ComponentId };
+import { LoroDoc } from "loro-crdt";
+
+type ComponentId = string;
+type Component<T> = { id: ComponentId; data: T };
+type ComponentDef<T> = ((data: T) => Component<T>) & {
+  id: ComponentId;
+  delete: () => ComponentDelete;
+};
+type ComponentDelete = { delete: ComponentId };
+type ComponentUpdate<T> = Component<T> | ComponentDelete;
+
+function defComponent<T>(id: string): ComponentDef<T> {
+  const constructor = (data: T) => ({ id, data });
+  constructor.id = id;
+  constructor.delete = () => ({ delete: id });
+  return constructor;
+}
 
 export class EntityId {
   bytes: Uint8Array;
@@ -36,4 +51,37 @@ export class EntityId {
   }
 }
 
-export class Entity {}
+class Entity {
+  id: EntityId;
+  components: { [id: ComponentId]: unknown };
+
+  constructor(...components: Component<unknown>[]) {
+    this.id = new EntityId();
+    this.components = {};
+    for (const { id, data } of components) {
+      this.components[id] = data;
+    }
+  }
+
+  update(...updates: ComponentUpdate<unknown>[]) {
+    for (const update of updates) {
+      if ("delete" in update) {
+        delete this.components[update.delete];
+      } else {
+        this.components[update.id] = update.data;
+      }
+    }
+  }
+
+  get<T>(def: ComponentDef<T>): T | undefined {
+    return this.components[def.id] as T | undefined;
+  }
+}
+
+const Name = defComponent<string>("name");
+const LastName = defComponent<string>("LastName");
+const Age = defComponent<number>("age");
+
+const ent = new Entity(Name("john"), Age(5));
+
+console.log(ent)
