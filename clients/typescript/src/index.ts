414b125e176ccb92b60ac6ec462b82df54cc164f
Zicklag <zicklag@katharostech.com>
2025-11-04 15:38:26 -0600

    wip: update TypeScript client to work with latest server.

--- clients/typescript/src/index.ts
+++ clients/typescript/src/index.ts
@@ -1,47 +1,123 @@
 import { io, Socket } from "socket.io-client";
 import parser from "socket.io-msgpack-parser";
+import { hex } from "@scure/base";
+import {
+  _void,
+  bool,
+  Bytes,
+  CodecType,
+  enhanceCodec,
+  Enum,
+  i64,
+  Option,
+  Result,
+  str,
+  Struct,
+  Tuple,
+  Vector,
+} from "scale-ts";
 
-export type IncomingEvent = {
-  stream: string;
-} & StreamEvent;
+const f64 = enhanceCodec(
+  Bytes(4),
+  (n: number) => {
+    let buffer = new ArrayBuffer(4);
+    let view = new DataView(buffer);
+    view.setFloat64(0, n, true);
+    return new Uint8Array(buffer);
+  },
+  (b) => {
+    let view = new DataView(b.buffer);
+    return view.getFloat64(0, true);
+  },
+);
 
-export type StreamEvent = {
-  idx: number;
-  user: string;
-  payload: ArrayBuffer;
-};
+export const Hash = enhanceCodec(Bytes(32), hex.decode, hex.encode);
+export const Ulid = enhanceCodec(Bytes(16), crockfordDecode, crockfordEncode);
 
-export type StreamGenesis = {
-  /** ULID that acts as a timestamp and unique identifier. */
-  stamp: string;
-  /** User account that created the stream. */
-  creator: string;
-  /**
-   * Hex-encoded module ID that the stream was created with.
-   *
-   * > **Note:** If there has been a module update since the stream was created this will not be the
-   * > current module.
-   */
-  module: string;
-  /** The parameters used to initialize the module when the stream was created. */
-  params: ArrayBuffer;
-};
+const SqlValue = Enum({
+  null: _void,
+  integer: i64,
+  real: f64,
+  text: str,
+  blob: Bytes(),
+});
+const SqlRow = Struct({
+  values: Vector(SqlValue),
+});
+export type SqlRows = CodecType<typeof SqlRows>;
+const SqlRows = Struct({
+  rows: Vector(SqlRow),
+  column_names: Vector(str),
+});
+
+export type IncomingEvent = CodecType<typeof IncomingEvent>;
+const IncomingEvent = Struct({
+  user: str,
+  payload: Bytes(),
+});
+
+export type LeafQuery = CodecType<typeof LeafQuery>;
+const LeafQuery = Struct({
+  query_name: str,
+  requesting_user: str,
+  params: Vector(Tuple(str, SqlValue)),
+});
+
+const StreamEventBatchArgs = Struct({
+  streamId: Hash,
+  events: Vector(IncomingEvent),
+});
+
+export type StreamQueryArgs = CodecType<typeof StreamQueryArgs>;
+const StreamQueryArgs = Struct({
+  streamId: Hash,
+  query: LeafQuery,
+});
+
+export type StreamQueryResp = CodecType<typeof StreamQueryResp>;
+const StreamQueryResp = Result(SqlRows, str);
+
+export type StreamSubscribeNotification = CodecType<
+  typeof StreamSubscribeNotification
+>;
+const StreamSubscribeNotification = Struct({
+  subscription_id: Ulid,
+  response: StreamQueryResp,
+});
+
+export type LeafSubscribeQuery = CodecType<typeof LeafSubscribeQuery>;
+const LeafSubscribeQuery = Struct({
+  query_name: str,
+  requesting_user: str,
+  params: Vector(Tuple(str, SqlValue)),
+  start: Option(i64),
+  batch_size: Option(i64),
+});
+export type StreamSubscribeArgs = CodecType<typeof StreamSubscribeArgs>;
+const StreamSubscribeArgs = Struct({
+  streamId: Hash,
+  query: LeafSubscribeQuery,
+});
+
+const HasWasmResp = Result(bool, str);
 
 type EventMap = {
   connect: () => void;
   disconnect: () => void;
   authenticated: (did: string) => void;
-  event: (event: IncomingEvent) => void;
   error: (error: string) => void;
 };
 
 export class LeafClient {
   socket: Socket;
+  #querySubscriptions: Map<
+    string,
+    (result: StreamQueryResp) => void | Promise<void>
+  > = new Map();
   _listeners: { [K in keyof EventMap]: EventMap[K][] } = {
     connect: [],
     disconnect: [],
     authenticated: [],
-    event: [],
     error: [],
   };
 
@@ -77,17 +153,13 @@ export class LeafClient {
     this.socket.on("error", (error) => {
       this.#emit("error", error);
     });
-    this.socket.on(
-      "stream/event",
-      (event: {
-        stream: string;
-        user: string;
-        idx: number;
-        payload: ArrayBuffer;
-      }) => {
-        this.#emit("event", event);
-      },
-    );
+    this.socket.on("stream/subscription_response", (data: Uint8Array) => {
+      const notification = StreamSubscribeNotification.dec(data);
+      const sub = this.#querySubscriptions.get(notification.subscription_id);
+      if (sub) {
+        sub(notification.response);
+      }
+    });
   }
 
   #emit<E extends keyof EventMap>(event: E, ...args: Parameters<EventMap[E]>) {
@@ -106,22 +178,28 @@ export class LeafClient {
     ) as any;
   }
 
-  async uploadModule(data: ArrayBuffer): Promise<string> {
-    const resp: { hash: string } | { error: string } =
-      await this.socket.emitWithAck("wasm/upload", data);
-    if ("error" in resp) {
-      throw new Error(resp.error);
+  async uploadModule(wasm_data: ArrayBuffer): Promise<string> {
+    const data: Uint8Array = await this.socket.emitWithAck(
+      "wasm/upload",
+      wasm_data,
+    );
+    const resp = Result(Hash, str).dec(data);
+    if (!resp.success) {
+      throw new Error(resp.value);
     }
-    return resp.hash;
+    return resp.value;
   }
 
   async hasModule(wasmId: string): Promise<boolean> {
-    const resp: { hasModule: boolean } | { error: string } =
-      await this.socket.emitWithAck("wasm/has", wasmId);
-    if ("error" in resp) {
-      throw new Error(resp.error);
+    const data: Uint8Array = await this.socket.emitWithAck(
+      "wasm/has",
+      Hash.enc(wasmId),
+    );
+    const resp = HasWasmResp.dec(data);
+    if (!resp.success) {
+      throw new Error(resp.value);
     }
-    return resp.hasModule;
+    return resp.value;
   }
 
   async createStream(
@@ -129,16 +207,16 @@ export class LeafClient {
     wasmId: string,
     params: ArrayBuffer,
   ): Promise<string> {
-    const resp: { streamId: string } | { error: string } =
-      await this.socket.emitWithAck("stream/create", {
-        ulid,
-        module: wasmId,
-        params,
-      });
-    if ("error" in resp) {
-      throw new Error(resp.error);
+    const data: Uint8Array = await this.socket.emitWithAck("stream/create", {
+      ulid,
+      module: wasmId,
+      params,
+    });
+    const resp = Result(Hash, str).dec(data);
+    if (!resp.success) {
+      throw new Error(resp.value);
     }
-    return resp.streamId;
+    return resp.value;
   }
 
   /** Helper to create a stream from a WASM module at a given URL, avoiding uploading / downloading
@@ -165,85 +243,49 @@ export class LeafClient {
     return await this.createStream(ulid, moduleId, params);
   }
 
-  async sendEvent(streamId: string, payload: ArrayBuffer): Promise<void> {
-    const resp: { ok: true } | { error: string } =
-      await this.socket.emitWithAck("stream/event", {
-        id: streamId,
-        payload,
-      });
-    if ("error" in resp) {
-      throw new Error(resp.error);
+  async sendEvent(streamId: string, event: IncomingEvent): Promise<void> {
+    this.sendEvents(streamId, [event]);
+  }
+
+  async sendEvents(streamId: string, events: IncomingEvent[]): Promise<void> {
+    const data: Uint8Array = await this.socket.emitWithAck(
+      "stream/event_batch",
+      StreamEventBatchArgs.enc({ streamId, events }),
+    );
+    const resp = Result(_void, str).dec(data);
+    if (!resp.success) {
+      throw new Error(resp.value);
     }
   }
 
-  async sendEvents(streamId: string, payloads: ArrayBuffer[]): Promise<void> {
-    const resp: { ok: true } | { error: string } =
-      await this.socket.emitWithAck("stream/event_batch", {
-        id: streamId,
-        payloads,
-      });
-    if ("error" in resp) {
-      throw new Error(resp.error);
-    }
-  }
-
-  async fetchEvents(
+  /** Returns a function that can be called to unsubscribe the query. */
+  async subscribe(
     streamId: string,
-    opts?: {
-      offset?: number;
-      limit?: number;
-      filter?: ArrayBuffer;
-    },
-  ): Promise<{ idx: number; user: string; payload: ArrayBuffer }[]> {
-    opts = { ...{ offset: 1, limit: 100 }, ...(opts || {}) };
-    // Remove any "undefined" values which get serialized and don't need to be sent over the
-    // network.
-    for (const key of Object.keys(opts) as (keyof typeof opts)[]) {
-      if (!opts[key]) {
-        delete opts[key];
+    query: LeafSubscribeQuery,
+    handler: (resp: StreamQueryResp) => Promise<void> | void,
+  ): Promise<() => Promise<void>> {
+    const data: Uint8Array = await this.socket.emitWithAck(
+      "stream/subscribe",
+      StreamSubscribeArgs.enc({ streamId, query }),
+    );
+    const resp = Result(Ulid, str).dec(data);
+    if (!resp.success) {
+      throw new Error(resp.value);
+    }
+
+    const subId = resp.value;
+    this.#querySubscriptions.set(subId, handler);
+    return async () => {
+      const data: Uint8Array = await this.socket.emitWithAck(
+        "stream/unsubscribe",
+        Ulid.enc(subId),
+      );
+      this.#querySubscriptions.delete(subId);
+      const resp = Result(bool, str).dec(data);
+      if (!resp.success) {
+        throw new Error(`Error unsubscribing stream query: ${resp.value}`);
       }
-    }
-    const resp:
-      | { events: { idx: number; user: string; payload: ArrayBuffer }[] }
-      | { error: string } = await this.socket.emitWithAck("stream/fetch", {
-      id: streamId,
-      ...opts,
-    });
-    if ("error" in resp) {
-      throw new Error(resp.error);
-    }
-    return resp.events;
-  }
-
-  async streamInfo(streamId: string): Promise<StreamGenesis> {
-    const resp: StreamGenesis | { error: string } =
-      await this.socket.emitWithAck("stream/info", {
-        stream_id: streamId,
-      });
-    if ("error" in resp) {
-      throw new Error(resp.error);
-    }
-    return resp;
-  }
-
-  async subscribe(streamId: string): Promise<void> {
-    const resp: { ok: true } | { error: string } =
-      await this.socket.emitWithAck("stream/subscribe", streamId);
-    if ("error" in resp) {
-      throw new Error(resp.error);
-    }
-  }
-
-  /**
-   * @returns whether or not the connection was previously subscribed.
-   */
-  async unsubscribe(streamId: string): Promise<boolean> {
-    const resp: { ok: true; wasUnSubscribed: boolean } | { error: string } =
-      await this.socket.emitWithAck("stream/unsubscribe", streamId);
-    if ("error" in resp) {
-      throw new Error(resp.error);
-    }
-    return resp.wasUnSubscribed;
+    };
   }
 
   disconnect() {
@@ -252,4 +294,65 @@ export class LeafClient {
       this._listeners[key as keyof typeof this._listeners] = [];
     }
   }
+
+  async query(streamId: string, query: LeafQuery): Promise<SqlRows> {
+    const data: Uint8Array = await this.socket.emitWithAck(
+      "stream/fetch",
+      StreamQueryArgs.enc({ streamId, query }),
+    );
+    const resp = StreamQueryResp.dec(data);
+    if (!resp.success) {
+      throw new Error(resp.value);
+    }
+    return resp.value;
+  }
+}
+
+// Code from https://github.com/perry-mitchell/ulidx/blob/5043c511406fb9b836ddf126583c80ffb90cbb73/source/crockford.ts
+// We already use ulidx but encoding is not exposed so we copy the functions here.
+const B32_CHARACTERS = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+export function crockfordEncode(input: Uint8Array): string {
+  const output: number[] = [];
+  let bitsRead = 0;
+  let buffer = 0;
+  const reversedInput = new Uint8Array(input.slice().reverse());
+  for (const byte of reversedInput) {
+    buffer |= byte << bitsRead;
+    bitsRead += 8;
+
+    while (bitsRead >= 5) {
+      output.unshift(buffer & 0x1f);
+      buffer >>>= 5;
+      bitsRead -= 5;
+    }
+  }
+  if (bitsRead > 0) {
+    output.unshift(buffer & 0x1f);
+  }
+  return output.map((byte) => B32_CHARACTERS.charAt(byte)).join("");
+}
+export function crockfordDecode(input: string): Uint8Array {
+  const sanitizedInput = input.toUpperCase().split("").reverse().join("");
+  const output: number[] = [];
+  let bitsRead = 0;
+  let buffer = 0;
+  for (const character of sanitizedInput) {
+    const byte = B32_CHARACTERS.indexOf(character);
+    if (byte === -1) {
+      throw new Error(
+        `Invalid base 32 character found in string: ${character}`,
+      );
+    }
+    buffer |= byte << bitsRead;
+    bitsRead += 5;
+    while (bitsRead >= 8) {
+      output.unshift(buffer & 0xff);
+      buffer >>>= 8;
+      bitsRead -= 8;
+    }
+  }
+  if (bitsRead >= 5 || buffer > 0) {
+    output.unshift(buffer & 0xff);
+  }
+  return new Uint8Array(output);
 }
